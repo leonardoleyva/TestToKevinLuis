97880ffb04e4e9b7b331b3a4c9cd6a98354ce73e
Leonardo Leyva <leonardo@indqtech.com>
2020-10-20 00:12:39 -0600

    Set .gitignore and simple functions

---
+++ functions.py
@@ -0,0 +1,18 @@
+import random
+
+def generateRandomNumbers (min: int, max: int):
+    return random.randint(min, max)
+
+def isEven (number: int):
+    return number % 2 == 0
+
+def factorial (number: int):
+    if number == 1: 
+        return 1
+    else:
+        return number * factorial(number - 1)
+
+def capitalizeFormat (string: str):
+    return string.capitalize()
+
+    
